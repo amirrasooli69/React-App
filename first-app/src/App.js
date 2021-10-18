275c8af7d22553c6e54f5bf82bd128048501a668
Amir <amirrasooli69@gmail.com>
2021-10-18 14:43:34 +0330

    reuseable component

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from "react";
+import Hi from "./Hi";
 
 class App extends Component {
     render(){
         return (
             <>
+            <Hi/>
             <div>
                 <h1>Amir</h1>
             </div>
+            <Hi/>
             <p>Hellow world</p>
             </>
         )
---
+++ first-app/src/Hi.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Hi extends Component {
+    render() {
+        return (
+            <>
+                <h1>Hi world</h1>
+                <p>welcome to the earth</p>
+            </>
+        )
+    }
+}
+
+export default Hi;
