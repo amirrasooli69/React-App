812d26d71e86a6c020c589e5f224799d07cef85d
Amir <amirrasooli69@gmail.com>
2021-10-18 22:14:59 +0330

    props in function component

--- first-app/src/Car.js
+++ first-app/src/Car.js
@@ -1,13 +1,23 @@
-import React , {Component} from "react";
+// import React , {Component} from "react";
+import React from "react";
 
-class Car extends Component {
-    render() {
-        return (
+
+// class Car extends Component {
+//     render() {
+//         return (
+//             <p>
+//                 My labtob brand is {this.props.brand} and it has core {this.props.cpu} cpu. {this.props.children}
+//             </p>
+//         )
+//     }
+// }
+
+const Car = (props) => {
+    return (
             <p>
-                My labtob brand is {this.props.brand} and it has core {this.props.cpu} cpu. {this.props.children}
+                My labtob brand is {props.brand} and it has core {props.cpu} cpu. {props.children}
             </p>
-        )
-    }
+    )
 }
 
 export default Car;
