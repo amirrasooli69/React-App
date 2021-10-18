c8200e8da3e65c0f7e23bfb7f693cd54e63775ac
Amir <amirrasooli69@gmail.com>
2021-10-18 20:54:40 +0330

    function component

--- first-app/src/Hi.js
+++ first-app/src/Hi.js
@@ -1,14 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Hi extends Component {
-    render() {
-        return (
-            <>
-                <h1>Hi world</h1>
-                <p>welcome to the earth</p>
-            </>
-        )
-    }
+// class Hi extends Component {
+//     render() {
+//         return (
+//             <>
+//                 <h1>Hi world</h1>
+//                 <p>welcome to the earth</p>
+//             </>
+//         )
+//     }
+// }
+
+const Hi = () => {
+    return (
+         <>
+            <h1>Hi world</h1>
+            <p>welcome to the earth</p>
+        </>
+    )
 }
 
 export default Hi;
