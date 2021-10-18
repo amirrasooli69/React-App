cd1e3304162d6b60d1a3f0cb05d51f6457f3e81a
Amir <amirrasooli69@gmail.com>
2021-10-18 21:03:59 +0330

    run javascript in html file

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,17 +1,12 @@
 import React, {Component} from "react";
-import Hi from "./Hi";
 
 class App extends Component {
+    number= 10;
     render(){
         return (
-            <>
-            <Hi/>
-            <div>
-                <h1>Amir</h1>
-            </div>
-            <Hi/>
-            <p>Hellow world</p>
-            </>
+
+            <p>multiple: {this.number * 2}</p>
+
         )
     }
 }
