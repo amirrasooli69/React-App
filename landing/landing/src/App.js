e0556ad72976cbd758733a1e892b635f12069b68
Amir <amirrasooli69@gmail.com>
2021-10-28 20:53:29 +0330

    fix footer

--- landing/landing/src/App.js
+++ landing/landing/src/App.js
@@ -6,6 +6,7 @@ import Banner from './components/Banner';
 import Cards from './components/Cards';
 import Search from './components/Search';
 import Logos from './components/Logos';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         <Cards />
         <Search />
         <Logos />
+        <Footer />
       </div>
     );
   }
