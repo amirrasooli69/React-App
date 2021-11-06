b92e3b2b50f5955a2217416c1daa8bf5f4a0ab53
amirrasooli <amirrasooli69@gmail.com>
2021-11-06 04:05:08 +0330

    add counter for products

--- landing/landing/src/components/Search.js
+++ landing/landing/src/components/Search.js
@@ -3,11 +3,29 @@ import React, { Component } from 'react';
 import styles from './Search.module.css'
 
 class Search extends Component {
+    constructor () {
+        super();
+        this.state = {
+            text: ""
+        };
+    }
+
+changeHandler = Event => {
+        this.setState({
+            text: Event.target.value,
+        }) 
+    }
     render() {
+        const {text}= this.state;
         return (
             <div className={styles.container}>
                 <p>Search what do you want</p>
-                <input placeholder='Search ...' />
+                <div>
+                    <input type='text' value={text} placeholder='Search ...' onChange={this.changeHandler}/>
+                    <br/>
+                    <br/>
+                    <span>{text}</span>
+                </div>
                 
             </div>
         );
