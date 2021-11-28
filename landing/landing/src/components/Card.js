bcd73a2580f5cd551788924842d59e0c7555d557
Amir <amirrasooli69@gmail.com>
2021-11-28 21:53:51 +0330

    get data from api

--- landing/landing/src/components/Card.js
+++ landing/landing/src/components/Card.js
@@ -35,7 +35,7 @@ class Card extends Component {
                 <h3>{name}</h3>
                 <p>{cost}{counter ? ` * ${counter} = ${counter * Number(cost.split(' ')[0])} $`:''}</p>
                 <div className={styles.counter}>
-                    <img className={! counter && styles.deactive} src={down} alt='arrow' onClick={this.downHandler}/>
+                    <img className={! counter ? styles.deactive: ""} src={down} alt='arrow' onClick={this.downHandler}/>
                     <span>{counter}</span>
                     <img src={up} alt='arrow' onClick={this.upHandler}/>
                 </div>               
