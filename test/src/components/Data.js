ac4f0c052707c71e8cb5862f97f6f5dff00bf48a
Amir <amirrasooli69@gmail.com>
2022-02-02 22:59:53 +0330

    use useReducer 2

--- test/src/components/Data.js
+++ test/src/components/Data.js
@@ -1,13 +1,20 @@
 import React, { useReducer } from 'react';
 
-const avalie = 0;
+const avalie = {
+    num1:0,
+    num2:5
+};
 
 const func = (current , action) => {
-    switch(action) {
+    switch(action.type) {
         case 'Up' : 
-            return current + 1;
+            return {...current , num1: current.num1 + action.value}
         case 'Down':
-            return current -1;
+            return {...current , num1: current.num1 - action.value}
+        case 'Up2' : 
+            return {...current , num2: current.num2 + action.value}
+        case 'Down2':
+            return {...current , num2: current.num2 - action.value}
         case 'Reset':
             return avalie;
         default: 
@@ -21,10 +28,16 @@ const Data = () => {
     const [number , dispatch] = useReducer(func , avalie)
     return (
         <div>
-            <h2>{number}</h2>
-            <button onClick={() => dispatch('Up')}>Up</button>
-            <button onClick={() => dispatch('Reset')}>Reset</button>
-            <button onClick={() => dispatch('Down')}>Down</button>
+            <h2>{number.num1}</h2>
+            <button onClick={() => dispatch({type:'Up' , value:3})}>Up</button>
+            <button onClick={() => dispatch({type :'Reset'})}>Reset</button>
+            <button onClick={() => dispatch({type:'Down' , value:3})}>Down</button>
+            <hr />
+
+            <h2>{number.num2}</h2>
+            <button onClick={() => dispatch({type:'Up2' , value: 1})}>Up</button>
+            <button onClick={() => dispatch({type :'Reset'})}>Reset</button>
+            <button onClick={() => dispatch({type:'Down2' , value: 1})}>Down</button>
 
         </div>
     );
